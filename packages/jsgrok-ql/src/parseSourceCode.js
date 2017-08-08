1a12f0520d264e7bbd7e4ba1f16e166279a4367d
Ahmad Amireh <ahmad@amireh.net>
2017-08-08 11:46:21 +0300

    don't use es6 sources to appease mocha

--- packages/jsgrok-ql/src/parseSourceCode.js
+++ packages/jsgrok-ql/src/parseSourceCode.js
@@ -1,10 +1,10 @@
-const acorn = require('acorn/dist/acorn.es.js');
-const interopRequire = module => module['__esModule'] && module['default'] && module['default'] || module;
+const acorn = require('acorn');
+const interopRequire = module => module['__esModule'] && module['default'] || module;
 
 interopRequire(require('acorn-jsx/inject.js'))(acorn);
 interopRequire(require('acorn-object-spread/inject.js'))(acorn);
 interopRequire(require('acorn-static-class-property-initializer/inject.js'))(acorn);
-interopRequire(require('acorn-dynamic-import/src/inject.js'))(acorn);
+interopRequire(require('acorn-dynamic-import/lib/inject.js'))(acorn);
 
 module.exports = function parseSourceCode(sourceCode) {
   return acorn.parse(sourceCode, {
